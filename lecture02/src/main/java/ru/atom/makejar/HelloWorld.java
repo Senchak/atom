f891992d0e425cfd7e721efb47e9b192ef2de254
DESKTOP-ANDRYHA\sench <senchakevuch@gmail.com>
2019-01-14 16:12:07 +0200

    my first commit

--- lecture02/src/main/java/ru/atom/makejar/HelloWorld.java
+++ lecture02/src/main/java/ru/atom/makejar/HelloWorld.java
@@ -6,6 +6,7 @@ package ru.atom.makejar;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println(getHelloWorld());
+        System.out.println(null == null);
     }
 
     public static String getHelloWorld() {
